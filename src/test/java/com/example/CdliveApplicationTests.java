c8bc28f4bcbdcc2e8c3f3030408f37113ed9cdc3
Josh Long <josh@joshlong.com>
2016-05-23 17:52:24 -0700

    adding maven

---
+++ src/test/java/com/example/CdliveApplicationTests.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.internal.exceptions.ExceptionIncludingMockitoWarnings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CdliveApplication.class)
+@WebAppConfiguration
+public class CdliveApplicationTests {
+
+	@Autowired
+	private WebApplicationContext wac  ;
+
+	private MockMvc mockMvc ;
+
+
+	@Before
+	public void before () {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	@Test
+	public void testHiAgain()  throws Exception{
+		this.mockMvc.perform(
+				MockMvcRequestBuilders.get("/hi")
+		)
+				.andExpect(status().isOk())
+				.andExpect(mvcResult ->
+						Assert.assertEquals(mvcResult.getResponse().getContentAsString(),("Hi"))) ;
+	}
+
+}
